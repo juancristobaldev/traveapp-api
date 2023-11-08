d62147d4029bb5bee57cc439c42a0eb9d3e52775
juancristobaldev <juancristobaldeveloper@gmail.com>
2023-11-08 16:51:30 -0300

    fix

--- passport/initPassport.js
+++ passport/initPassport.js
@@ -29,7 +29,7 @@ const initPassport = (app) => {
         clientID: process.env["GOOGLE_CLIENT_ID"],
         clientSecret: process.env["GOOGLE_SECRET_CLIENT_ID"],
         callbackURL: process.env["GOOGLE_CALLBACK_URL"],
-        scope: ["profile"],
+        scope: ["profile", "email"],
       },
       async (accessToken, refreshToken, profile, cb) => {
         console.log(profile);
