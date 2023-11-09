e7f793794067d852f23178bf6e5919ab5c366f05
juancristobaldev <juancristobaldeveloper@gmail.com>
2023-11-08 22:58:03 -0300

    fix

--- passport/initPassport.js
+++ passport/initPassport.js
@@ -26,19 +26,18 @@ const initPassport = (app) => {
   passport.use(
     new GoogleStrategy.OAuth2Strategy(
       {
-        clientID:
-          "594477246406-an4gbfsndslvng9gdd10u4vkjjmmec0i.apps.googleusercontent.com",
-        clientSecret: "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",
-        callbackURL: "https://traveapp-api.vercel.app/oauth2/redirect/google",
-        scope: ["profile", "email"],
+        clientID: process.env["GOOGLE_CLIENT_ID"],
+        clientSecret: process.env["GOOGLE_SECRET_CLIENT_ID"],
+        callbackURL: process.env["GOOGLE_CALLBACK_URL"],
+        scope: ["profile"],
       },
       async (accessToken, refreshToken, profile, cb) => {
         console.log(profile);
 
         const oauth2Client = new google.auth.OAuth2(
-          "594477246406-an4gbfsndslvng9gdd10u4vkjjmmec0i.apps.googleusercontent.com",
-          "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",
-          "https://traveapp-api.vercel.app/oauth2/redirect/google"
+          process.env["GOOGLE_CLIENT_ID"],
+          process.env["GOOGLE_SECRET_CLIENT_ID"],
+          process.env["GOOGLE_CALLBACK_URL"]
         );
 
         const data = {
